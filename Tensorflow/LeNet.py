fd1bcf99230355a3b7d01a4050eef2634eee42aa
JeHwan MA <38252059+mjh1583@users.noreply.github.com>
2021-01-17 22:09:10 +0900

    CNN_기초-LeNet(CNN)

---
+++ Tensorflow/LeNet.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import pandas as pd
+
+# 데이터 준비하기
+(indep, dep), _ = tf.keras.datasets.mnist.load_data()
+print(indep.shape, dep.shape)
+
+indep = indep.reshape(60000, 28, 28, 1)
+dep = pd.get_dummies(dep)  # 원핫인코딩
+print(indep.shape, dep.shape)
+
+# CNN(Convolution Nerual Network) 모델의 구조 만들기
+X = tf.keras.layers.Input(shape=[28, 28, 1])
+
+H = tf.keras.layers.Conv2D(6, kernel_size=5, padding='same', activation='swish')(X)  # 3개의 특징맵 =  3채널의 특징맵
+H = tf.keras.layers.MaxPool2D()(H)
+H = tf.keras.layers.Conv2D(16, kernel_size=5, activation='swish')(H)  # 6개의 특징맵 =  6채널의 특징맵
+H = tf.keras.layers.MaxPool2D()(H)
+
+H = tf.keras.layers.Flatten()(H)  # 표로 만듦
+H = tf.keras.layers.Dense(120, activation='swish')(H)
+H = tf.keras.layers.Dense(84, activation='swish')(H)
+Y = tf.keras.layers.Dense(10, activation='softmax')(H)
+
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy', metrics='accuracy')
+
+# 모델 학습
+model.fit(indep, dep, epochs=10)
+
+# 모델 이용
+pred = model.predict(indep[0:5])
+print(pd.DataFrame(pred).round(2))  # 예측
+
+print(dep[0:5])  # 답
+
+# 모델 알아보기
+print(model.summary())
+
+
+# ----------------------------------------------------------------------
+# 데이터 준비하기
+(indep, dep), _ = tf.keras.datasets.cifar10.load_data()
+print(indep.shape, dep.shape)
+
+dep = pd.get_dummies(dep.reshape(50000))  # 원핫인코딩(표 형태이므로 reshape 한 후)
+print(indep.shape, dep.shape)
+
+# CNN(Convolution Nerual Network) 모델의 구조 만들기
+X = tf.keras.layers.Input(shape=[32, 32, 3])
+
+H = tf.keras.layers.Conv2D(6, kernel_size=5, activation='swish')(X)  # 3개의 특징맵 =  3채널의 특징맵
+H = tf.keras.layers.MaxPool2D()(H)
+H = tf.keras.layers.Conv2D(16, kernel_size=5, activation='swish')(H)  # 6개의 특징맵 =  6채널의 특징맵
+H = tf.keras.layers.MaxPool2D()(H)
+
+H = tf.keras.layers.Flatten()(H)  # 표로 만듦
+H = tf.keras.layers.Dense(120, activation='swish')(H)
+H = tf.keras.layers.Dense(84, activation='swish')(H)
+Y = tf.keras.layers.Dense(10, activation='softmax')(H)
+
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy', metrics='accuracy')
+
+# 모델 학습
+model.fit(indep, dep, epochs=10)
+
+# 모델 이용
+pred = model.predict(indep[0:5])
+print(pd.DataFrame(pred).round(2))  # 예측
+
+print(dep[0:5])  # 답
+
+# 모델 알아보기
+print(model.summary())
